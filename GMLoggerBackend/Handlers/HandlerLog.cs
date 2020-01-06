7df26c493faad2f66dd5877bfef231d55ffc9255
OneZeroZeroOneOne <50696667+OneZeroZeroOneOne@users.noreply.github.com>
2020-01-06 14:09:15 +0200

    log

--- GMLoggerBackend/Handlers/HandlerLog.cs
+++ GMLoggerBackend/Handlers/HandlerLog.cs
@@ -10,19 +10,11 @@ namespace GMLoggerBackend.Handlers
 {
     class HandlerLog : IHandler
     {
-        public Dictionary<string, string> Process(BufferStream buffer, SocketHelper mySocket, Dictionary<string, string> data)
+        public Dictionary<string, string> Process(BaseModel model, BufferStream buffer, SocketHelper mySocket, Dictionary<string, string> data)
         {
-<<<<<<< Updated upstream
-            var model = new LogModelRequest();
-            model.FromBuffer(buffer);
-            Console.WriteLine(model.msg);
-=======
-            //var model = new LogModelRequest();
-            //model.FromBuffer(buffer);
             
             var this_model = model.ToModel<LogModelRequest>();
             Console.WriteLine(this_model.msg);
->>>>>>> Stashed changes
             return data;
 
         }
