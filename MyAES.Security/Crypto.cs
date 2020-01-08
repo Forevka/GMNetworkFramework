b2333e3ab93f7838bf0b24543c15ca31651f4049
Forevka <zebestforevka@gmail.com>
2020-01-09 00:45:47 +0200

    added crypto

---
+++ MyAES.Security/Crypto.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAES.Security
+{
+    public class MyAesCrypto
+    {
+        #region Settings
+
+        private static int _iterations = 2;
+        private static int _keySize = 256;
+
+        private static string _hash = "SHA1";
+
+        private Encoding myEncoding = Encoding.UTF8;
+
+        private string _salt; // Random
+        private string _vector; // Random
+
+        private byte[] _saltBytes;
+        private byte[] _vectorBytes;
+
+        private string _password;
+
+        #endregion
+
+        public MyAesCrypto(string password, string salt, string vector, Encoding encoding)
+        {
+            myEncoding = encoding;
+
+            _password = password;
+
+            _salt = salt;
+            _vector = vector;
+
+            _saltBytes = myEncoding.GetBytes(salt.ToArray());
+            _vectorBytes = myEncoding.GetBytes(vector.ToArray());
+        }
+
+        public MyAesCrypto(string password, string salt, string vector)
+        {
+            _password = password;
+
+            _salt = salt;
+            _vector = vector;
+
+            _saltBytes = myEncoding.GetBytes(salt.ToArray());
+            _vectorBytes = myEncoding.GetBytes(vector.ToArray());
+        }
+
+        public byte[] Encrypt(byte[] value)
+        {
+            return Encrypt<AesManaged>(value, _password);
+        }
+
+        public byte[] Encrypt<T>(byte[] valueBytes, string password) where T : SymmetricAlgorithm, new()
+        {
+            byte[] encrypted;
+            using (T cipher = new T())
+            {
+                PasswordDeriveBytes _passwordBytes =
+                    new PasswordDeriveBytes(password, _saltBytes, _hash, _iterations);
+                byte[] keyBytes = _passwordBytes.GetBytes(_keySize / 8);
+
+                cipher.Mode = CipherMode.CBC;
+
+                using (ICryptoTransform encryptor = cipher.CreateEncryptor(keyBytes, _vectorBytes))
+                {
+                    using (MemoryStream to = new MemoryStream())
+                    {
+                        using (CryptoStream writer = new CryptoStream(to, encryptor, CryptoStreamMode.Write))
+                        {
+                            writer.Write(valueBytes, 0, valueBytes.Length);
+                            writer.FlushFinalBlock();
+                            encrypted = to.ToArray();
+                        }
+                    }
+                }
+                cipher.Clear();
+            }
+
+            return encrypted;
+        }
+
+        public byte[] Decrypt(byte[] value)
+        {
+            return Decrypt<AesManaged>(value, _password);
+        }
+
+        public byte[] Decrypt<T>(byte[] valueBytes, string password) where T : SymmetricAlgorithm, new()
+        {
+            byte[] decrypted;
+            int decryptedByteCount = 0;
+
+            using (T cipher = new T())
+            {
+                PasswordDeriveBytes _passwordBytes = new PasswordDeriveBytes(password, _saltBytes, _hash, _iterations);
+                byte[] keyBytes = _passwordBytes.GetBytes(_keySize / 8);
+
+                cipher.Mode = CipherMode.CBC;
+
+                try
+                {
+                    using (ICryptoTransform decryptor = cipher.CreateDecryptor(keyBytes, _vectorBytes))
+                    {
+                        using (MemoryStream from = new MemoryStream(valueBytes))
+                        {
+                            using (CryptoStream reader = new CryptoStream(from, decryptor, CryptoStreamMode.Read))
+                            {
+                                decrypted = new byte[valueBytes.Length];
+                                decryptedByteCount = reader.Read(decrypted, 0, decrypted.Length);
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return Array.Empty<byte>();
+                }
+
+                cipher.Clear();
+            }
+
+            return decrypted;
+        }
+
+    }
+}
---
+++ MyEAS/Program.cs
@@ -0,0 +1,50 @@
+﻿using MyAES.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEAS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string salt = "aselrias38490a32";
+            string vector = "8947az34awl34kjq";
+
+            string pass = "abcde";
+
+            Encoding utf8 = Encoding.UTF8;
+
+            var a = new MyAesCrypto(pass, salt, vector);
+
+            byte[] data = utf8.GetBytes("hello world".ToArray());
+
+            Console.WriteLine(utf8.GetString(data, 0, data.Count()));
+
+            foreach (var ch in data)
+                Console.Write(ch + " ");
+
+            Console.WriteLine();
+
+            var encrypted = a.Encrypt(data);
+
+            foreach (var ch in encrypted)
+                Console.Write(ch + " ");
+
+            Console.WriteLine();
+
+            var decrypted = a.Decrypt(encrypted);
+
+            foreach (var ch in decrypted)
+                Console.Write(ch + " ");
+
+            Console.WriteLine();
+
+            Console.WriteLine(utf8.GetString(decrypted, 0, decrypted.Count()));
+            Console.ReadLine();
+        }
+    }
+}
