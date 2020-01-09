36ffbc3a7a6605d45af032c1dc3465f42a2b5e71
Forevka <zebestforevka@gmail.com>
2020-01-09 22:20:39 +0200

    fixing assembly info

--- GMNetworkFramework/Properties/AssemblyInfo.cs
+++ GMNetworkFramework/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("GMNetworkFramework")]
+[assembly: AssemblyTitle("GMNetworkFramework.Export")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GMNetworkFramework")]
+[assembly: AssemblyProduct("GMNetworkFramework.Export")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
