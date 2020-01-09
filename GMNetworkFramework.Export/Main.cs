9886cc300850329c6930793cb2e0be35eebea902
Forevka <zebestforevka@gmail.com>
2020-01-09 23:27:21 +0200

    better work

--- GMNetworkFramework.Export/Main.cs
+++ GMNetworkFramework.Export/Main.cs
@@ -4,9 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GMNetworkFramework
+namespace GMNetworkFramework.Export
 {
     class Main
     {
+        [DllExport]
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        [DllExport]
+        public static double Minus(double a, double b)
+        {
+            return a - b;
+        }
+
+        [DllExport]
+        public static double Mult(double a, double b)
+        {
+            return a * b;
+        }
+
+        [DllExport]
+        public static string StringTest(string a, string b)
+        {
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            return a + b;
+        }
     }
 }
