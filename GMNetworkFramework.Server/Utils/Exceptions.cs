898d003f18f94c2aa33c0409305a4a1057bb54d1
Forevka <zebestforevka@gmail.com>
2020-01-09 22:55:49 +0200

    rename and last fixes

--- GMNetworkFramework.Server/Utils/Exceptions.cs
+++ GMNetworkFramework.Server/Utils/Exceptions.cs
@@ -1,15 +1,16 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace GMNetworkFramework.Server.Exceptions
 {
-    class CancelHandlerException : Exception
+    class CancelHandlerException : Exception, ISerializable
     {
         public CancelHandlerException(string message) : base(message)
         {
         }
     }
 
-    class StopProcessingException : Exception
+    class StopProcessingException : Exception, ISerializable
     {
         public StopProcessingException(string message) : base(message)
         {
